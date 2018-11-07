42e5cf2cedcedc3dbfedd98b1dec8aa85da69fbd
ham948 <44324380+ham948@users.noreply.github.com>
2018-11-07 22:16:22 +0000

    Add files via upload

---
+++ arduinocont.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  2 12:14:03 2018
+
+@author: Daniel
+"""
+
+import serial
+import time
+
+red= 0
+run= 1
+s= serial.Serial()
+s.baudrate=9600
+s.port='COM13'
+pxlLine=open("C:\\ardct\\test.txt",'r')
+s.open()
+while run:
+    red=int(s.read(1))
+    if red==8:
+        pxln=pxlLine.read(11)
+        if pxln[0]=='4':
+            run=0
+        else:
+                s.write(pxln.encode())
+    
+s.close()
--- plygnMirr.ino
+++ plygnMirr.ino
@@ -4,30 +4,55 @@
  */
 
 #define BAUD 9600
-#define led 13
 
+int newtime=0;
+int oldtime=0;
+int times=0;
 
-String receivedString;
-volatile int newtime=0;
-volatile int oldtime=0;
-volatile int times=0;
+int lnewtime=0;
+int loldtime=0;
+int ltimes=0;
+
 int pulse;
-int a=0;
+char pxl[11];/*={1,1,0,0,1,0,1,1,0,1};*/
 
 void setup() {
-  Serial.begin(BAUD);
-  pinMode(led, OUTPUT);
   attachInterrupt (0, cycletime, RISING);
   pinMode(4, OUTPUT);
 }
 
+void nextline(){
+  Serial.begin(BAUD);
+  Serial.print(8);
+      delay(100);
+  for(int i=0;i<10&&Serial.available();i++){
+    delay(50);
+    pxl[i]=Serial.read();
+  }
+  Serial.end();
+}
+
 void loop() {
-for(int n=0;n!=20;n++){
-pulse=times/20;
-digitalWrite(4,HIGH);
-delayMicroseconds(pulse);
-digitalWrite(4,LOW);
-delayMicroseconds(pulse);}
+ pulse=times/22;
+for(int i=0;i<10;i++){
+  if(pxl[i]==1){
+    digitalWrite(4,HIGH);
+    delayMicroseconds(pulse);
+  }
+  else{
+  digitalWrite(4,LOW);
+  delayMicroseconds(pulse);}
+  }
+
+
+
+
+lnewtime=millis();
+ltimes=lnewtime-loldtime;
+if(ltimes>=1000){
+  loldtime=lnewtime;
+  nextline();
+  }
 }
 void cycletime(){
 newtime=micros();
